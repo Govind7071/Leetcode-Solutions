025e91ab5d4e7d06105cde189f4bad01dc3bb42f
govind <yadavgovind707166@gmail.com>
2025-11-29 19:32:25 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20251129_193225.cpp
@@ -0,0 +1,17 @@
+// Last updated: 29/11/2025, 19:32:25
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        for(int i=0;i<nums.size();i++){
+5            for(int j=i+1;j<nums.size();j++){
+6                if((nums[i]+nums[j])==target){
+7                   return {i,j}; 
+8                }
+9            }
+10
+11        
+12        }
+13      return {};  
+14    }
+15    
+16};
